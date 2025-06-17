42861fb12a6447d851d9217489a87502900cfdca
Geninnd <geninnd@outlook.com>
2025-06-17 12:47:48 -0300

    teste final sync argo

--- src/main.py
+++ src/main.py
@@ -5,7 +5,7 @@ app = FastAPI(title="Infra Challenge App")
 
 @app.get("/")
 async def home():
-    return {"message": "Hello World - Teste 3 17/06 webhook argo"}
+    return {"message": "Hello World - Teste 4 sync argo"}
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
